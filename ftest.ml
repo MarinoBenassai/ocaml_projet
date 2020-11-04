8fa877fc780c48dd4c42e37f8cc29b8af1ee0df7
benassai <benassai@etud.insa-toulouse.fr>
2020-11-04 06:24:29 -0500

    Session 2

--- ftest.ml
+++ ftest.ml
@@ -1,5 +1,6 @@
 open Gfile
-    
+open Tools
+
 let () =
 
   (* Check the number of command-line arguments *)
@@ -11,10 +12,10 @@ let () =
 
 
   (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
-  
+
   let infile = Sys.argv.(1)
   and outfile = Sys.argv.(4)
-  
+
   (* These command-line arguments are not used for the moment. *)
   and _source = int_of_string Sys.argv.(2)
   and _sink = int_of_string Sys.argv.(3)
@@ -24,7 +25,7 @@ let () =
   let graph = from_file infile in
 
   (* Rewrite the graph that has been read. *)
-  let () = write_file outfile graph in
+  let () = write_file outfile (gmap graph (fun x -> string_of_int( 2* int_of_string x))) in
 
-  ()
+    ()
 
--- tools.ml
+++ tools.ml
@@ -1,3 +1,8 @@
-let remove_single_arcs ((a:Graph.id),(b:'a Graph.out_arcs)) = (a,[]) 
+open Graph;;
 
-let clone_nodes (g:'a Graph.graph)  = List.map remove_single_arcs g
+let clone_nodes g = n_fold g new_node empty_graph ;;
+
+(*let change_weight id1 id2 w = *)
+let new_arc2 f g id1 id2 w  = new_arc g id1 id2 (f w);;
+
+let gmap g f = e_fold g (new_arc2 f) (clone_nodes g);;
